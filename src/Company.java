9c223339b3823a3e20afaeddf7a94f6ef1c5d0cb
alzhong99 <31635541+alzhong99@users.noreply.github.com>
2017-09-12 22:53:59 -0700

    Update Company.java

--- src/Company.java
+++ src/Company.java
@@ -2,14 +2,8 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 public class Company {
-	private String name;
 	private ArrayList<Employee> employees;
-	private HashMap<String, Executive> departmentToExecutive;
-	public Company(String name) {
-		this.name = name;
-		this.employees = new ArrayList<Employee> ();
-		this.departmentToExecutive = new HashMap<String, Executive> ();
-	}
+	private HashMap<String, Employee> departmentToExecutive;
 	public void addEmployee(Employee employee) {
 		if(employee.getClass().getName() == "Executive") {
 			String department = InputHelper.readString("What department is this executive in charge of?");
@@ -17,18 +11,19 @@ public class Company {
 		}
 		employees.add(employee);
 	}
-	//Returns string with company name, departments, and executives
-	public String toString() {
-		String retVal = name + " Company has departments:\n";
-		for(String department: departmentToExecutive) {
-			retVal = retVal + "\t[" + department + " headed by " + departmentToExecutive.get(department).getName() + "]\n";
+	//Prints company departments and executives
+	public void printDepartments() {
+		System.out.println("Company has departments:");
+		for(String department: departmentToExecutive.keySet()) {
+			System.out.println("\t" + department + " headed by " + departmentToExecutive.get(department).getName());
 		}
-		return retVal;
 	}
-	public String getName() {
-		return name;
+	//Prints company employees
+	public void printAllEmployees() {
+		System.out.println("Company has employees " + employees);
 	}
-	public ArrayList<Employee> getEmployees() {
-		return employees;
+
+	public static void main(String[] args) {
+		
 	}
 }
