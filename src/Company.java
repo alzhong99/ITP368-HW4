c5da238c50fa048d1de9c03d89b6bcae96a10bfd
alzhong99 <31635541+alzhong99@users.noreply.github.com>
2017-09-12 23:05:10 -0700

    Update Company.java

--- src/Company.java
+++ src/Company.java
@@ -2,24 +2,35 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 public class Company {
-	private ArrayList<Employee> employees;
-	private HashMap<String, Employee> departmentToExecutive;
-	public void addEmployee(Employee employee) {
-		if(employee.getClass().getName() == "Executive") {
+	private static ArrayList<Employee> employees;
+	private static HashMap<String, Employee> departmentToExecutive;
+	
+	//Creates and assigns new task
+	public static void giveTask() {
+		//Receive input to create new task
+		employee.assignTask(new Task());
+	}	
+	//Adds employee and maps to department if employee is executive
+	public static void addEmployee() {
+		Employee employee = null;
+		//Receive input to create new employee
+		
+		//Use this code if creating new executive
+		/* 
 			String department = InputHelper.readString("What department is this executive in charge of?");
 			departmentToExecutive.put(department, employee);
-		}
+		*/
 		employees.add(employee);
 	}
 	//Prints company departments and executives
-	public void printDepartments() {
+	public static void printDepartments() {
 		System.out.println("Company has departments:");
 		for(String department: departmentToExecutive.keySet()) {
 			System.out.println("\t" + department + " headed by " + departmentToExecutive.get(department).getName());
 		}
 	}
 	//Prints company employees
-	public void printAllEmployees() {
+	public static void printAllEmployees() {
 		System.out.println("Company has employees " + employees);
 	}
 
